d105882c7b2e737fe67e2ed5ed640745e1352c42
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-15 11:08:57 +0600

    op.py

--- op.py
+++ op.py
@@ -1,6 +1,6 @@
 import os,time
-print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
-time.sleep(30)
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+time.sleep(5)
 import ok
 ok.main()
  
