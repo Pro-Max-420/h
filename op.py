c88c0b8d0f6d7f49c41759b2594cf08cf9d4e23f
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-16 09:41:01 +0600

    op.py

--- op.py
+++ op.py
@@ -1,9 +1,6 @@
 import os,time
-os.system('clear')
 print('[>] Checking Updates...')
-os.system('git pull')
 print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
 time.sleep(5)
 import ok
 ok.main()
- 
