3c4d4c07d0c7f60bc97545a0b8d4a4941d827af5
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 01:16:26 +0600

    op.py

--- op.py
+++ op.py
@@ -1,5 +1,7 @@
 import os,time
-print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
 time.sleep(5)
+print('\033[1;34m\n JOIN MY GIFT GROUP Tools\033[1;37m')
+time.sleep(3)
 import ok
 ok.main()
